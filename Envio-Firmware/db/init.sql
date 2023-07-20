6913f259d0e819c00b72fe899b9b804051f313c2
Guilherme Brida <guilherme.brida@crearesistemas.com.br>
2023-07-20 16:29:09 -0300

    db files

---
+++ Envio-Firmware/db/init.sql
@@ -0,0 +1,24 @@
+-- CREATE SCHEMA copilotos;
+CREATE TABLE IF NOT EXISTS public.vozes
+(
+    "IMEI" text COLLATE pg_catalog."default" NOT NULL,
+    "SN" text COLLATE pg_catalog."default" NOT NULL,
+    "VOZES" bigint,
+    reception_datetime timestamp with time zone DEFAULT timezone('America/Sao_Paulo', now())
+    );
+INSERT INTO public.vozes ("IMEI","SN","VOZES") VALUES ('teste','teste',22);
+INSERT INTO public.vozes ("IMEI","SN","VOZES") VALUES ('teste2','teste2',25);
+INSERT INTO public.vozes ("IMEI","SN","VOZES") VALUES ('teste3','teste3',12);
+INSERT INTO public.vozes ("IMEI","SN","VOZES") VALUES ('teste4','teste4',3);
+INSERT INTO public.vozes ("IMEI","SN","VOZES") VALUES ('teste5','teste5',19);
+
+
+CREATE TABLE IF NOT EXISTS public.firmware
+(
+    "device_id" text COLLATE pg_catalog."default" NOT NULL,
+    "SN" text COLLATE pg_catalog."default" NOT NULL,
+    "content_blocs" bytea,
+    "blocs_acks" bytea,
+    send_datetime timestamp with time zone DEFAULT timezone('America/Sao_Paulo', now()),
+    reception_datetime timestamp with time zone DEFAULT timezone('America/Sao_Paulo', now())
+    );
