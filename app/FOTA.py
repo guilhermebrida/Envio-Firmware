cd6d1cc6c7197af01eaffdc4ece914254a1befe6
Guilherme Brida <guilherme.brida@crearesistemas.com.br>
2023-08-15 13:14:25 -0300

    threads

--- app/FOTA.py
+++ app/FOTA.py
@@ -287,7 +287,7 @@ async def main():
             ip_equipamento = addr[0]
             print(data,ip_equipamento)
             device_id = send_ack(sock, addr, data)
-            if device_id in ids_desatualizados[0]:
+            if device_id in ids_desatualizados:
                 print(device_id, ids_desatualizados[0])
                 print(device_id in ids_desatualizados[0])
                 solicitar_serial_number(sock, device_id, addr)
